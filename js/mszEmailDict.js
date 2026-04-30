cf88332416fd905fc223714097b51c3feb1d850c
Sunil R. <sunil.ratnayake12@gmail.com>
2026-05-01 00:21:12 +0200

    usunąłem email Sikorskiego z mszEmailDict

--- js/mszEmailDict.js
+++ js/mszEmailDict.js
@@ -3,6 +3,6 @@
 
 email_dict = {
     "MSZ":{
-        "wszyscy": ["biuro@radeksikorski.pl", "biuro@platforma.org", "radoslaw.sikorski@europarl.europa.eu", "j.siekierski@radeksikorski.eu", "p.napolski@radeksikorski.eu", "petycje.skargi.wnioski@msz.gov.pl", "BM.Sekretariat@msz.gov.pl", "dpe.sekretariat@msz.gov.pl", "dpzue.sekretariat@msz.gov.pl", "DPB.Sekretariat@msz.gov.pl", "radoslaw.sikorski@msz.gov.pl", "DK.Sekretariat@msz.gov.pl", "kancelaria@msz.gov.pl", "rzecznik@msz.gov.pl", "wbartoszewski.sekretariat@msz.gov.pl", "mbosacki.sekretariat@msz.gov.pl", "iniemczycki.sekretariat@msz.gov.pl", "rkupiecki.sekretariat@msz.gov.pl", "hmoscickadendys.sekretariat@msz.gov.pl", "wzajaczkowski.sekretariat@msz.gov.pl"]
+        "wszyscy": ["biuro@radeksikorski.pl", "biuro@platforma.org", "j.siekierski@radeksikorski.eu", "p.napolski@radeksikorski.eu", "petycje.skargi.wnioski@msz.gov.pl", "BM.Sekretariat@msz.gov.pl", "dpe.sekretariat@msz.gov.pl", "dpzue.sekretariat@msz.gov.pl", "DPB.Sekretariat@msz.gov.pl", "radoslaw.sikorski@msz.gov.pl", "DK.Sekretariat@msz.gov.pl", "kancelaria@msz.gov.pl", "rzecznik@msz.gov.pl", "wbartoszewski.sekretariat@msz.gov.pl", "mbosacki.sekretariat@msz.gov.pl", "iniemczycki.sekretariat@msz.gov.pl", "rkupiecki.sekretariat@msz.gov.pl", "hmoscickadendys.sekretariat@msz.gov.pl", "wzajaczkowski.sekretariat@msz.gov.pl"]
     }
 }
