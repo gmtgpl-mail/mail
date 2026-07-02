66ba8b08d2509b48403238d02e0fe718d5350438
Sunil R. <sunil.ratnayake12@gmail.com>
2026-07-02 10:51:14 +0200

    dodanie maila o Kareemie

---
+++ js/UMWrEmailDict.js
@@ -0,0 +1,5 @@
+email_dict = {
+    "Władze UMWr": {
+        "wszyscy": ["rektor@umw.edu.pl", "rektor@umw.edu.pl", "dg@umw.edu.pl", "dl@umw.edu.pl", "kancelaria@mnisw.gov.pl", "sekretariat.bm@mnisw.gov.pl", "sekretariat.dsw@mnisw.gov.pl",]
+    }
+}
