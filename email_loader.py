b8eccf869d516e9fcc40bcbed9be39ed35218bc6
sunil <sunil.ratnayake12@gmail.com>
2026-08-27 21:53:49 +0200

    email loader update

--- email_loader.py
+++ email_loader.py
@@ -2,17 +2,37 @@ import csv
 
 
 def main():
-    with open('convertcsv.csv') as f:
+    with open('baza_maili_posłowie_i_senatorowie.csv') as f:
         dane = [{k: v for k, v in row.items()}
             for row in csv.DictReader(f, skipinitialspace=True)]
     email_string = ""
-    parties = set()
+    email_set = set()
+    count = 0
+    emails_in_each_part = 99
+    total_parts = 1
+    # parties = set()
     for osoba in dane:
-        parties.add(osoba['nationalPoliticalGroup'])
-        if osoba['zaimki:'] == 'M' and osoba['notatki'] == '' and osoba['nationalPoliticalGroup'] == "Konfederacja Korony Polskiej":
-            email_string += str("\""+osoba['e-mail:']+"\", ")
+        # parties.add(osoba['nationalPoliticalGroup'])
+        # if osoba['zaimki:'] == 'M' and osoba['notatki'] == '' and osoba['nationalPoliticalGroup'] == "Konfederacja Korony Polskiej":
+        # print(osoba.keys())
+        if osoba['posada:'] == 'poseł':
+            personEmails = osoba['e-mail:'].split(';')
+            # print(len(personEmails))
+            for email in personEmails:
+                count += 1
+                email_string += str("\""+email.strip()+"\", ")
+                if email.strip() in email_set:
+                    print(email)
+                email_set.add(email.strip())
+                if count % emails_in_each_part == 0:
+                    email_string += '\n'
+                    total_parts += 1
     email_string = email_string.replace(";", "\", \"")
     print(email_string)
-    print(parties)
+    print('email count: '+str(count))
+    print('email set len: '+str(len(email_set)))
+    print('total count: '+str(len(dane)))
+    print('total parts: '+str(total_parts))
+    # print(parties)
 
 main()
